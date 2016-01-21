f436c53b6e658e9591108fb37d1a56e4ffaf65cc
Sacha Best <sachabest@outlook.com>
2016-01-20 22:49:22 -0500

    Initial commit with Maya library project config

---
+++ cis099/Test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <maya/MSimple.h>
+#include <maya/MIOStream.h>
+
+DeclareSimpleCommand(HelloWorld, "Autodesk", "2016");
+
+MStatus HelloWorld::doIt(const MArgList&)
+{
+	std::cout << "Hello World\n" << std::endl;
+	return MS::kSuccess;
+}
